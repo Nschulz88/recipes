21ae4dd9e71e8df3a773aca411b1a2a59a3dd239
Natalie Schulz <natalie.schulz@me.com>
2019-10-15 16:52:35 -0700

    Initialized expo starter app.

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default App = () => {
+  return (
+    <View>
+      <Text>Starter App Structure</Text>
+    </View>
+  );
+}
